f7af881f9dbc11a2609b68b1a1f84d796150ce3f
Daniil325 <potekhindaniil@mail.ru>
2021-03-09 18:09:30 +0700

    ееее роккк

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //еееее роккк
+        c_result.setOnClickListener { appendOnExpression("лол", true) }
+        c_expression.setOnClickListener { appendOnExpression("кек", true) }
 
 
         //Числа
