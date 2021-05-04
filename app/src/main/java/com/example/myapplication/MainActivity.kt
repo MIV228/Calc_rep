1cc69a584ff9ecda585147b85394743309b8736e
MIV228 <mivgithub@gmail.com>
2021-05-04 18:09:10 +0700

    tytiohmiylgomk;

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -27,21 +27,23 @@ class MainActivity : AppCompatActivity() {
         c_zero.setOnClickListener { appendOnExpression("0", true) }
         c_dot.setOnClickListener { appendOnExpression(".", true) }
         //Операции
-        c_plus.setOnClickListener { appendOnExpression("+", false)
+        c_plus.setOnClickListener {
+            appendOnExpression("+", false)
         }
-        c_minus.setOnClickListener { appendOnExpression("-", false)
+        c_minus.setOnClickListener {
+            appendOnExpression("-", false)
         }
-        c_multi.setOnClickListener { appendOnExpression("*", false)
-
+        c_multi.setOnClickListener {
+            appendOnExpression("*", false)
         }
-        c_div.setOnClickListener { appendOnExpression("/", false)
-
+        c_div.setOnClickListener {
+            appendOnExpression("/", false)
         }
-        c_open.setOnClickListener { appendOnExpression("(", false)
-
+        c_open.setOnClickListener {
+            appendOnExpression("(", false)
         }
-        c_close.setOnClickListener { appendOnExpression(")", false)
-
+        c_close.setOnClickListener {
+            appendOnExpression(")", false)
         }
         c_ac.setOnClickListener {
             c_expression.text = ""
@@ -50,39 +52,38 @@ class MainActivity : AppCompatActivity() {
         c_backspace.setOnClickListener {
             val string = c_expression.text.toString()
             if (string.isNotEmpty()) {
-                c_expression.text = string.substring(0, string.length-1)
+                c_expression.text = string.substring(0, string.length - 1)
             }
             c_result.text = ""
         }
-        c_equals.setOnClickListener{
-            try{
+        c_equals.setOnClickListener {
+            try {
                 val expression = ExpressionBuilder(c_expression.text.toString()).build()
                 val result = expression.evaluate()
                 val longResult = result.toLong()
-                if(result == longResult.toDouble())
+                if (result == longResult.toDouble())
                     c_result.text = longResult.toString()
                 else
                     c_result.text = result.toString()
-            }
-            catch (e:Exception){
-                Log.d("Exception"," message " +e.message)
+            } catch (e: Exception) {
+                Log.d("Exception", " message " + e.message)
             }
         }
     }
+
     fun appendOnExpression(string: String, canClear: Boolean) {
-            if (c_result.text.isNotEmpty()) {
-                c_expression.text = ""
-            }
-            if (canClear) {
-                c_result.text = ""
-                c_expression.append(string)
-            }
-            else {
-                c_expression.append(c_result.text)
-                c_expression.append(string)
-                c_result.text = ""
-            }
+        if (c_result.text.isNotEmpty()) {
+            c_expression.text = ""
         }
+        if (canClear) {
+            c_result.text = ""
+            c_expression.append(string)
+        } else {
+            c_expression.append(c_result.text)
+            c_expression.append(string)
+            c_result.text = ""
+        }
+    }
 //            if (c_expression.text.length - 1() = "1" || "2" ) {
 //                c_expression.text.length - 1
 //                c_expression.text = "+"
