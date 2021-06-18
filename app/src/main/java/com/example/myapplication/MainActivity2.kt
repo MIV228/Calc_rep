2a8b9840c901ef7c84e559843bc65c8dd55120b4
MIV228 <mivgithub@gmail.com>
2021-06-18 23:20:19 +0700

    fixed white theme

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,7 +22,7 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2) //лол!!!111! это была самая тупая ошибка лол!!!
 
 
-        val per = findViewById<Button>(R.id.pereh)
+        val per = findViewById<Button>(R.id.w_pereh)
         per.setOnClickListener {
             val perehod = Intent(this, MainActivity::class.java)
             startActivity(perehod)
@@ -30,108 +30,106 @@ class MainActivity2 : AppCompatActivity() {
 
          try {
 //еееее роккк
-             main.setOnClickListener{ appendOnExpression("", true) }
-             c_result.setOnClickListener{ appendOnExpression("", true) }
-             c_expression.setOnClickListener{ appendOnExpression("", true) }
+             w_main.setOnClickListener{ appendOnExpression("", true) }
+             w_c_result.setOnClickListener{ appendOnExpression("", true) }
+             w_c_expression.setOnClickListener{ appendOnExpression("", true) }
              //Числа
-             c_one.setOnClickListener{ appendOnExpression("1", true)
+             w_c_one.setOnClickListener{ appendOnExpression("1", true)
                  animate(it)
              }
-             c_two.setOnClickListener{ appendOnExpression("2", true)
+             w_c_two.setOnClickListener{ appendOnExpression("2", true)
                  animate(it)
              }
-             c_three.setOnClickListener{ appendOnExpression("3", true)
+             w_c_three.setOnClickListener{ appendOnExpression("3", true)
                  animate(it)
              }
-             c_four.setOnClickListener{ appendOnExpression("4", true)
+             w_c_four.setOnClickListener{ appendOnExpression("4", true)
                  animate(it)
              }
-             c_five.setOnClickListener{ appendOnExpression("5", true)
+             w_c_five.setOnClickListener{ appendOnExpression("5", true)
                  animate(it)
              }
-             c_six.setOnClickListener{ appendOnExpression("6", true)
+             w_c_six.setOnClickListener{ appendOnExpression("6", true)
                  animate(it)
              }
-             c_seven.setOnClickListener{ appendOnExpression("7", true)
+             w_c_seven.setOnClickListener{ appendOnExpression("7", true)
                  animate(it)
              }
-             c_eight.setOnClickListener{ appendOnExpression("8", true)
+             w_c_eight.setOnClickListener{ appendOnExpression("8", true)
                  animate(it)
              }
-             c_nine.setOnClickListener{ appendOnExpression("9", true)
+             w_c_nine.setOnClickListener{ appendOnExpression("9", true)
                  animate(it)
              }
-             c_zero.setOnClickListener{ appendOnExpression("0", true)
+             w_c_zero.setOnClickListener{ appendOnExpression("0", true)
                  animate(it)
              }
-             c_dot.setOnClickListener{ appendOnExpression(".", true)
+             w_c_dot.setOnClickListener{ appendOnExpression(".", true)
                  animate(it)
              }
              //Операции
-             c_plus.setOnClickListener{ appendOnExpression("+", false)
+             w_c_plus.setOnClickListener{ appendOnExpression("+", false)
                  animate(it)
              }
-             c_minus.setOnClickListener{ appendOnExpression("-", false)
+             w_c_minus.setOnClickListener{ appendOnExpression("-", false)
                  animate(it)
              }
-             c_multi.setOnClickListener{ appendOnExpression("*", false)
+             w_c_multi.setOnClickListener{ appendOnExpression("*", false)
                  animate(it)
              }
-             c_div.setOnClickListener{ appendOnExpression("/", false)
+             w_c_div.setOnClickListener{ appendOnExpression("/", false)
                  animate(it)
              }
-             c_open.setOnClickListener{ appendOnExpression("(", false)
+             w_c_open.setOnClickListener{ appendOnExpression("(", false)
                  animate(it)
              }
-             c_close.setOnClickListener{ appendOnExpression(")", false)
+             w_c_close.setOnClickListener{ appendOnExpression(")", false)
                  animate(it)
              }
-             c_ac.setOnClickListener {
+             w_c_ac.setOnClickListener {
                  animate(it)
-                 c_expression.text = ""
-                 c_result.text = ""
+                 w_c_expression.text = ""
+                 w_c_result.text = ""
              }
-             c_backspace.setOnClickListener{
+             w_c_backspace.setOnClickListener{
                  animate(it)
-                 val string = c_expression.text.toString()
+                 val string = w_c_expression.text.toString()
                  if (string.isNotEmpty()) {
-                     c_expression.text = string.substring(0, string.length - 1)
+                     w_c_expression.text = string.substring(0, string.length - 1)
                  }
-                 c_result.text = ""
+                 w_c_result.text = ""
              }
-             c_equals.setOnClickListener {
+             w_c_equals.setOnClickListener {
                  animate(it)
                  try {
-                     val expression = ExpressionBuilder(c_expression.text.toString()).build()
+                     val expression = ExpressionBuilder(w_c_expression.text.toString()).build()
                      val result = expression.evaluate()
                      val longResult = result.toLong()
                      if (result == longResult.toDouble())
-                         c_result.text = longResult.toString()
+                         w_c_result.text = longResult.toString()
                      else
-                         c_result.text = result.toString()
+                         w_c_result.text = result.toString()
                  } catch (e: Exception) {
                      Log.d("Exception", " message " + e.message)
                  }
-                 animate(c_result)
+                 animate(w_c_result)
              }
          }
          catch (e:Exception) {
              Log.d("Exception", " message " + e.message)
-
-
          }}
 
     fun appendOnExpression(string: String, canClear: Boolean) {
-        if (c_result.text.isNotEmpty()) {
-            c_expression.text = ""
+        if (w_c_result.text.isNotEmpty()) {
+            w_c_expression.text = ""
         }
         if (canClear) {
-            c_result.text = ""
-            c_expression.append(string)
+            w_c_result.text = ""
+            w_c_expression.append(string)
         } else {
-            c_expression.append(c_result.text)
-            c_expression.append(string)
-            c_result.text = ""
+            w_c_expression.append(w_c_result.text)
+            w_c_expression.append(string)
+            w_c_result.text = ""
         }
     }
 
